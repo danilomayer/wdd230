7a61cad0c442ad8c5168257700ceac803f107459
danilomayer <danilomayer2020@gmail.com>
2024-03-07 19:31:22 -0300

    deleted duplicated function on links.js

--- scripts/links.js
+++ scripts/links.js
@@ -4,14 +4,8 @@ const linksURL = "https://danilomayer.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-}
-
-getLinks();
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    displayLinks(data);
+    displayLinks(data)
 }
 
 getLinks();
