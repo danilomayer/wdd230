e0ac69ff3815bd26ee32ea9482d6829a380eca83
danilomayer <danilomayer2020@gmail.com>
2024-03-08 01:15:52 -0300

    links using fetch JSON updated, almost done

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,27 @@
 const baseURL = "https://danilomayer.github.io/wdd230/";
 const linksURL = "https://danilomayer.github.io/wdd230/data/links.json";
+const ul = document.querySelector('ul');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
-    displayLinks(data)
+    //displayLinks(data);
+
+    const displayLinks = (weeks) => {
+        weeks.forEach((week) => {
+            let card = document.createElement('li');
+            let link = document.createElement('a');
+
+            link.textContent = `${week.title}`;
+
+            link.setAttribute('href', week.url);
+
+            card.appendChild(link);
+
+            ul.appendChild(card);
+        });
+    }
+    displayLinks(data.weeks);
 }
 
 getLinks();
