3ee6f6934bfb5daee9df8ba25c4b98e2c213d6d7
danilomayer <danilomayer2020@gmail.com>
2024-04-23 13:54:44 -0300

    unfinished weather exercise

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,7 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid={API key}';
+
+//step2.2 is not understandable
+
